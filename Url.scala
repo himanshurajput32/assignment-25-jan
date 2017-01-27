86bc86f37a17b2ec480ded511ef05760fe9a9aff
himanshurajput32 <himanshu.rajput32@gmail.com>
2017-01-27 15:13:23 +0530

    Update Url.scala

--- Url.scala
+++ Url.scala
@@ -8,14 +8,14 @@ object URL{
      }
 
   def unapply(url:String):Option[(String , String, String,Map[String,String])] = {
-   val parts= url.split("?")
+   val parts= url.split("\\?")
    if(parts.length==2){
    val parts_1=parts(0).split("://")
    val protocol=parts_1(0)
    val parts_2=parts_1(1).split("/")
    val domain=parts_2(0)
    val path="/"+parts_2(1)+"/"+parts_2(2)
-   val temp=parts(1).split("= | &") 
+   val temp=parts(1).split("=|&") 
    val params=Map(temp(0)->temp(1),temp(2)->temp(3),temp(4)->temp(5))
    Some(protocol,domain,path,params) 
    }
